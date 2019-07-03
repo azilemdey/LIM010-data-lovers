701aa322122fa5df79e492ccbd7025e04f9ea7d8
andreamoscoso <ams_t2@hotmail.com>
2019-07-03 01:43:16 -0500

    subiendo estilos de las paginas 2 y 3

--- src/main.js
+++ src/main.js
@@ -4,19 +4,73 @@ const button = document.getElementById('boton');
 button.addEventListener('click', () => {
   const pass = document.getElementById('pass').value;
   const userEmail = document.getElementById('email').value;
-  if (pass == 'LABORATORIA') {
-    let email = document.getElementById("viewEmail");
-    email.innerHTML = `Su email: ${userEmail}`;
+  if (pass === 'LABORATORIA') {
+    let email = document.getElementById('view-email');
+    email.innerHTML = `<i class="fa fa-user" aria-hidden="true"></i> ${userEmail}`;
     document.getElementById('contenedor').classList.add('hide');
-    document.getElementById('viewEmail').classList.remove('hide')
-    document.getElementById('contenedor1').classList.remove('hide')
+    document.getElementById('cabecera').classList.remove('hide');
+    document.getElementById('view-email').classList.remove('hide');
+    document.getElementById('contenedor1').classList.remove('hide');
     document.getElementById('page1').classList.remove('hide');
   } else {
-    document.getElementById("texto1").classList.add('estilo');
+    document.getElementById('texto1').classList.add('estilo');
     i = i + 1;
-    } if (i < 3) {
-      document.getElementById("pass").disabled = true;
-      document.getElementById("email").disabled = true;
-      document.getElementById("boton").disabled = true;
-    }
+  } if (i < 3) {
+    document.getElementById('pass').disabled = true;
+    document.getElementById('email').disabled = true;
+    document.getElementById('boton').disabled = true;
+  }
 });
+
+
+const selector = document.getElementById('country');
+selector.addEventListener('change', () =>{
+  for (i = 0; i < selector.length; i++) {
+    if (selector.selectedIndex === 1) {
+      document.getElementById('contenedor5').classList.add('hide');
+      document.getElementById('page5').classList.add('hide');
+      document.getElementById('contenedor4').classList.add('hide');
+      document.getElementById('page4').classList.add('hide');
+      document.getElementById('contenedor3').classList.add('hide');
+      document.getElementById('page3').classList.add('hide');
+      document.getElementById('contenedor2').classList.remove('hide');
+      document.getElementById('page2').classList.remove('hide');
+      document.getElementById('contenedor1').classList.add('hide');
+      document.getElementById('page1').classList.add('hide');
+    } else if (selector.selectedIndex === 2) {
+      document.getElementById('contenedor5').classList.add('hide');
+      document.getElementById('page5').classList.add('hide');
+      document.getElementById('contenedor4').classList.add('hide');
+      document.getElementById('page4').classList.add('hide');
+      document.getElementById('contenedor3').classList.remove('hide');
+      document.getElementById('page3').classList.remove('hide');
+      document.getElementById('contenedor2').classList.add('hide');
+      document.getElementById('page2').classList.add('hide');
+      document.getElementById('contenedor1').classList.add('hide');
+      document.getElementById('page1').classList.add('hide');
+    } else if (selector.selectedIndex === 3) {
+      document.getElementById('contenedor5').classList.add('hide');
+      document.getElementById('page5').classList.add('hide');
+      document.getElementById('contenedor4').classList.remove('hide');
+      document.getElementById('page4').classList.remove('hide');
+      document.getElementById('contenedor3').classList.add('hide');
+      document.getElementById('page3').classList.add('hide');
+      document.getElementById('contenedor2').classList.add('hide');
+      document.getElementById('page2').classList.add('hide');
+      document.getElementById('contenedor1').classList.add('hide');
+      document.getElementById('page1').classList.add('hide');
+    } else if (selector.selectedIndex === 4) {
+      document.getElementById('contenedor5').classList.remove('hide');
+      document.getElementById('page5').classList.remove('hide');
+      document.getElementById('contenedor4').classList.add('hide');
+      document.getElementById('page4').classList.add('hide');
+      document.getElementById('contenedor3').classList.add('hide');
+      document.getElementById('page3').classList.add('hide');
+      document.getElementById('contenedor2').classList.add('hide');
+      document.getElementById('page2').classList.add('hide');
+      document.getElementById('contenedor1').classList.add('hide');
+      document.getElementById('page1').classList.add('hide');
+    }
+  }
+});
+
