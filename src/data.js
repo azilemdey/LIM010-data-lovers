283a5988e3c84b8a9807760e10eea472fd6efdfe
azilemdey <azilem.v.d@gmail.com>
2019-07-05 02:42:00 -0500

    tratando de imprimir en select

--- src/data.js
+++ src/data.js
@@ -3,8 +3,16 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/* const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example; */
+
+let arrayvacio= new Array();
+for(let i=0; i<=138;i++) {
+arrayvacio[i]=WORLDBANK.PER.indicators[i].indicatorName;}
+
+  console.log(arrayvacio);
+
+
